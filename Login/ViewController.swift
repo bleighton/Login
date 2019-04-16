c0372c3b4120f7037c0f3bf0366934075679dec8
SoftDev Student <softdevstudent@robotics-mba-06.local>
2019-04-16 12:33:06 -0400

    Initial Commit

---
+++ Login/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by SoftDev Student on 4/16/19.
+//  Copyright © 2019 Belle L. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
